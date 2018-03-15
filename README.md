40fb55aa1531622ab5029db87597a87ce01c88fc
Geoff's Macbook <geoffsmacbook@lawn-143-215-114-177.lawn.gatech.edu>
2018-03-15 18:13:15 -0400

    version 1.0 - Creation of file structure, game mechanics, and initial look and feel

---
+++ README.md
@@ -0,0 +1,92 @@
+# Trivia Game (of Thrones)
+
+* **Created:** Saturday, March 10th, 2018
+
+* **Context:** This is the fifth week's homework assignment as a part of the Georgia Tech Coding Bootcamp. In this assignment, we are constructing a Trivia Game using a combination of HTML, CSS, Javascript, and jQuery. The primary focus of the assignment is an understanding of the utility of time-based functions.  The homework's high level instructions are as follows:
+
+```
+In this assignment, You'll create a Trivia game using JavaScript for the logic and jQuery to manipulate HTML. Be sure to layout this app with valid HTML and stylish CSS.
+```
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+```
+* This is a web page exercise.  As such, you're welcome to download a copy and run it locally for review purposes.  
+
+* Otherwise, you can view the output at: https://geoff-goodwin-dev.github.io/Crystals-Collector-Game/
+```
+
+### Prerequisites
+
+These are things you need to install the software and how to install them:
+
+```
+Any modern web browser should be able to open this static website
+```
+
+### Installing
+
+These are a set of step-by-step series of examples that tell you have to get a development env running:
+
+```
+Not Applicable
+```
+
+## Running the unit-level tests
+
+These are the details of how to run the automated tests for this system:
+
+```
+Not Applicable
+```
+
+### End-to-end tests
+
+These are the details of what these tests test and why:
+
+```
+Not Applicable
+```
+
+### And coding style tests
+
+This is where you will find details outlining the approach and coding style in terms of  what these tests test and why
+
+```
+Not Applicable
+```
+
+## Deployment
+
+Additional notes about how to deploy this on a live system can be found here:
+
+```
+Not Applicable: This is a static website for educational purposes and therefore is suggested not to be deployed outside of its current location on Github
+```
+
+## Built With
+
+* [Webstorm](https://www.jetbrains.com/webstorm/) (Version: 2017.3.4) - Generation of HTML, CSS, and JavaScript
+
+## Contributing
+
+* Geoff Goodwin (self) - Author
+
+## Versioning
+
+* version 1.0 - Creation of file structure, game mechanics, and initial look and feel
+
+## Authors
+
+* **Geoff Goodwin** - *Initial work* - [Geoff-Goodwin-Dev](https://github.com/Geoff-Goodwin-Dev)
+
+## License
+
+details TBD
+
+## Acknowledgments
+
+* Appreciation to [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2) for initial Readme-Template
+* Use of the Fisher–Yates Shuffle Function made shuffling the questions as well as the answers within the questions possible
---
+++ assets/javascript/app.js
@@ -0,0 +1,187 @@
+// VARIABLE DECLARATIONS
+var correct = 0;
+var incorrect = 0;
+var answerDisplayOrder = [1, 2, 3, 4];
+var questionsObjArray = [];
+var questionCounter = 0;
+var q01;
+var q02;
+var q03;
+var q04;
+var q05;
+var q06;
+var q07;
+var q08;
+var q09;
+var q10;
+var q11;
+var q12;
+var q13;
+var q14;
+var q15;
+var q16;
+var q17;
+var q18;
+var q19;
+var q20;
+var timer;
+var start;
+var countDownDurationS;
+var secondsRemaining;
+var unusedSeconds = 0;
+
+$(document).ready(function() {
+
+  // Fisher–Yates Shuffle Function
+  function fYShuffle(array) {
+    var m = array.length, t, i;
+    while (m) { // While there remain elements to shuffle…
+      i = Math.floor(Math.random() * m--); // Pick a remaining element…
+      t = array[m]; // And swap it with the current element.
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
+
+  // Trivia Question Object Constructor Function
+  function TriviaQuestion (id, question, answer1, answer2, answer3, answer4) {
+    this.id = id;
+    this.question = question;
+    this.answer1 = answer1;
+    this.answer2 = answer2;
+    this.answer3 = answer3;
+    this.answer4 = answer4;
+    this.correct = answer1;
+    // this.logQuestion = function () {
+    //   console.log("Question ID:", this.id);
+    //   console.log("Question:", this.question);
+    //   console.log("Answer 1 (correct):", this.answer1);
+    //   console.log("Answer 2:", this.answer2);
+    //   console.log("Answer 3:", this.answer3);
+    //   console.log("Answer 4:", this.answer4);
+    //   console.log("--------------");
+    // };
+  }
+
+  // TRIVIA QUESTION OBJECT GENERATION
+  function questionCreation () {
+    q01 = new TriviaQuestion("01", "What is the surname given to bastards born in Dorne?", "Sand", "Snow", "Snake", "Silt");
+    questionsObjArray.push(q01);
+    q02 = new TriviaQuestion("02", 'Which character is nicknamed “The Hound?"', "Sandor Clegane", "Gregor Clegane", "Canile Clegane", "Roger Clegane");
+    questionsObjArray.push(q02);
+    q03 = new TriviaQuestion("03", "Who is the Lord Commander of the Kingsguard at the beginning of Game of Thrones?", "Ser Barristan Selmy", "Ser Jamie Lannister", "Ser Loras Tyrell", "Ser Jeor Mormont");
+    questionsObjArray.push(q03);
+    q04 = new TriviaQuestion("04", "To whom was Margery Tyrell never married?", "Stannis Baratheon", "Joffrey Baratheon", "Renly Baratheon", "Tommen Baratheon");
+    questionsObjArray.push(q04);
+    q05 = new TriviaQuestion("05", 'Who was known as “The King Beyond the Wall?”', "Mance Rayder", "Jon Snow", "Maester Aemon", "Benjen Jon");
+    questionsObjArray.push(q05);
+    q06 = new TriviaQuestion("06", "How many times did Sansa Stark get married?", "Twice", "Never", "Once", "Three times");
+    questionsObjArray.push(q06);
+    q07 = new TriviaQuestion("07", "How is Lyanna Mormont related to Jorah Mormont?", "Cousin", "Daughter", "Wife", "Niece");
+    questionsObjArray.push(q07);
+
+    questionsObjArray = fYShuffle(questionsObjArray);
+  }
+
+  // TIMER FUNCTION PLAY
+  function countDownTimer(){
+    var countDownDurationMS = countDownDurationS * 1000;
+    var diff = Date.now() - start; //determines difference between start time and current time
+    secondsRemaining = Math.ceil((countDownDurationMS - diff) / 1000); //determines total seconds remaining in countdown
+    var m = Math.floor(secondsRemaining / 60); //determines minutes part of total seconds remaining in countdown
+    var mm; //converts single digit minutes to contain leading zero
+    if (m < 10) {
+      mm = "0" + m;
+    } else {
+      mm = m;
+    }
+    var s = secondsRemaining - m * 60; // determines seconds part of total seconds remaining in countdown
+    var ss;
+    if (s < 10) {
+      ss = "0" + s;
+    } else {
+      ss = s;
+    }
+    $("#timer").text(mm + ':' + ss);
+    if(secondsRemaining <= 0) {
+      alert("time ran out");
+      incorrect++;
+      $("#losses").text(incorrect);
+      questionCounter++;
+      nextQuestion();
+      return;
+    }
+    timer = setTimeout(countDownTimer,1000);
+  }
+
+  // UPDATE DISPLAY OF THE TRIVIA QUESTION FUNCTION
+  function updateQuestionDisplay() {
+    $("#question").text(questionsObjArray[questionCounter].question);
+    answerDisplayOrder = fYShuffle(answerDisplayOrder);
+    console.log("shuffled order for answers", answerDisplayOrder);
+    $("#answer" + answerDisplayOrder[0]).text(questionsObjArray[questionCounter].answer1);
+    $("#answer" + answerDisplayOrder[1]).text(questionsObjArray[questionCounter].answer2);
+    $("#answer" + answerDisplayOrder[2]).text(questionsObjArray[questionCounter].answer3);
+    $("#answer" + answerDisplayOrder[3]).text(questionsObjArray[questionCounter].answer4);
+  }
+
+  // GAMEPLAY START BUTTON CLICKED FUNCTION
+  $("#start").click(function() {
+    questionCreation();
+    updateQuestionDisplay();
+    $("#questionTextContainer").css("display", "block");
+    $("#startState").css("display", "none");
+
+    countDownDurationS = 15;
+    start = Date.now(); //sets the starting time for the timer
+    countDownTimer(); //sets the timer in motion
+  });
+
+  // CLICKING AN ANSWER BUTTON FUNCTION
+  $("#questionTextContainer").on("click", ".answerButton", function () {
+    var selectedAnswer = $(this).text();
+    var correctAnswer = questionsObjArray[questionCounter].correct;
+    console.log("correct Answer", correctAnswer);
+    console.log("Selected Answer Text", selectedAnswer);
+
+    // Storage of unused seconds for game end stats and stopping of timer
+    unusedSeconds = unusedSeconds + secondsRemaining;
+    console.log("unused seconds:", unusedSeconds);
+    clearTimeout(timer);
+
+    // Checking for Correct Answer
+    if (selectedAnswer === correctAnswer) {
+      alert("correct");
+      correct++;
+      $("#wins").text(correct);
+    }
+    else {
+      alert("incorrect");
+      incorrect++;
+      $("#losses").text(incorrect);
+    }
+    questionCounter++;
+    nextQuestion();
+  });
+
+  function nextQuestion() {
+    if (questionCounter === 5) {
+      alert("Game Complete")
+    }
+    else {
+      updateQuestionDisplay();
+      countDownDurationS = 15;
+      start = Date.now(); //sets the starting time for the timer
+      countDownTimer(); //sets the timer in motion
+    }
+  }
+});
+
+// MODAL RELATED FUNCTIONS
+function openInstructions() {
+  $("#modalInstruction").css("display", "block");
+}
+function closeInstructions() {
+  $("#modalInstruction").css("display", "none");
+}
